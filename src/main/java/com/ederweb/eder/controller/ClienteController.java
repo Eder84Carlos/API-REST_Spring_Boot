ea226772316edf71bb826635dc7546e42bb900e0
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-01-25 14:57:42 -0300

    Projeto Simples

--- src/main/java/com/ederweb/eder/controller/ClienteController.java
+++ src/main/java/com/ederweb/eder/controller/ClienteController.java
@@ -1,16 +1,36 @@
 package com.ederweb.eder.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ederweb.eder.model.Cliente;
+import com.ederweb.eder.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping
-	public String hello() {
-		return "Olá mundo";
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
+
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
